ae8ceadd452a3887ffac9ff7048843ceb8848daf
xinhuixu <x.xu.1102@gmail.com>
2017-04-22 21:32:09 -0400

    union

--- set.py
+++ set.py
@@ -1,10 +1,12 @@
 A = [1,2,3]
-B =['a','b','c',1]
+B =[2,3,4]
 
 #set of all objects in A, B, or both
 def union(A, B):
     return A + [x for x in B if x not in A]
 
 
+print 'A: ', A
+print 'B: ', B
 #test cases
-print union(A, B)
+print 'union', union(A, B)
