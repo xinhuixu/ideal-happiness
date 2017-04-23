623c5a5a3eff5130f3ff98b6251fabba9e154cd6
xinhuixu <x.xu.1102@gmail.com>
2017-04-22 21:37:23 -0400

    inter & setdiff

--- set.py
+++ set.py
@@ -1,12 +1,21 @@
 A = [1,2,3]
 B =[2,3,4]
 
+#assume there is no duplicate value in a set; mathematically it wouldn't make sense anyways
+
 #set of all objects in A, B, or both
 def union(A, B):
     return A + [x for x in B if x not in A]
 
+def intersection(A, B):
+    return [x for x in A if x in B]
 
+def set_diff(A, B):
+    return [x for x in A if x not in B]
+    
 print 'A: ', A
 print 'B: ', B
 #test cases
 print 'union', union(A, B)
+print 'intersection', intersection(A, B)
+print 'set_diff', set_diff(A, B)
